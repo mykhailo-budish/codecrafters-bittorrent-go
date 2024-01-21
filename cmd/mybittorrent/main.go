c3a3970f75dfe4bf305387be1257e1209ad18eed
budish.mikhail <budish.mikhail@steelkiwi.com>
2024-01-21 17:26:59 +0200

    Compute ingo hash

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"unicode"
 )
 
-func decodeString(bencodedString string) (string, int, error) {
+func _decodeString(bencodedString string) (string, int, error) {
 	firstColonIndex := 0
 
 	for bencodedString[firstColonIndex] != ':' {
@@ -27,7 +29,7 @@ func decodeString(bencodedString string) (string, int, error) {
 	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], partLength, nil
 }
 
-func decodeInteger(bencodedNumber string) (int, int, error) {
+func _decodeInteger(bencodedNumber string) (int, int, error) {
 	numberEnd := 1
 	for bencodedNumber[numberEnd] != 'e' {
 		numberEnd++
@@ -48,16 +50,17 @@ func decodeInteger(bencodedNumber string) (int, int, error) {
 	return number, partLength, nil
 }
 
-func decodeList(bencodedList string) (interface{}, int, error) {
+func _decodeList(bencodedList string) ([]interface{}, int, error) {
+	emptyList := make([]interface{}, 0)
 	if bencodedList == "le" {
-		return []bool{}, 0, nil
+		return emptyList, 0, nil
 	}
 	var elements []interface{}
 	lastElementEndIndex := 0
 	for bencodedList[lastElementEndIndex+1] != 'e' {
 		element, elementLength, err := decodeBencodeData(bencodedList[lastElementEndIndex+1:])
 		if err != nil {
-			return "", 0, err
+			return emptyList, 0, err
 		}
 		elements = append(elements, element)
 		lastElementEndIndex += elementLength
@@ -65,20 +68,21 @@ func decodeList(bencodedList string) (interface{}, int, error) {
 	return elements, lastElementEndIndex + 2, nil
 }
 
-func decodeDict(bencodedDict string) (interface{}, int, error) {
+func _decodeDict(bencodedDict string) (map[string]interface{}, int, error) {
+	emptyDict := make(map[string]interface{})
 	if bencodedDict == "de" {
-		return map[string]bool{}, 0, nil
+		return emptyDict, 0, nil
 	}
-	dict := make(map[string]interface{})
+	dict := emptyDict
 	lastElementEndIndex := 0
 	for bencodedDict[lastElementEndIndex+1] != 'e' {
-		key, keyLength, err := decodeString(bencodedDict[lastElementEndIndex+1:])
+		key, keyLength, err := _decodeString(bencodedDict[lastElementEndIndex+1:])
 		if err != nil {
-			return "", 0, err
+			return emptyDict, 0, err
 		}
 		value, valueLength, err := decodeBencodeData(bencodedDict[lastElementEndIndex+keyLength+1:])
 		if err != nil {
-			return "", 0, err
+			return emptyDict, 0, err
 		}
 		dict[key] = value
 		lastElementEndIndex += keyLength + valueLength
@@ -88,31 +92,81 @@ func decodeDict(bencodedDict string) (interface{}, int, error) {
 
 func decodeBencodeData(bencodedString string) (interface{}, int, error) {
 	if unicode.IsDigit(rune(bencodedString[0])) {
-		return decodeString(bencodedString)
+		return _decodeString(bencodedString)
 	}
 
 	if bencodedString[0] == 'i' {
-		return decodeInteger(bencodedString)
+		return _decodeInteger(bencodedString)
 	}
 
 	if bencodedString[0] == 'l' {
-		return decodeList(bencodedString)
+		return _decodeList(bencodedString)
 	}
 
 	if bencodedString[0] == 'd' {
-		return decodeDict(bencodedString)
+		return _decodeDict(bencodedString)
 	}
 
 	return "", 0, fmt.Errorf("unsupported type")
 }
 
-type TorrentFile struct {
-	Announce string
-	Info     struct {
-		Length      int
-		Name        string
-		PieceLength int `bencode:"piece length"`
-		Pieces      string
+func _encodeString(stringToEncode string) string {
+	strLength := len(stringToEncode)
+	return fmt.Sprintf("%d:%s", strLength, stringToEncode)
+}
+
+func _encodeInteger(numberToEncode int) string {
+	return fmt.Sprintf("i%de", numberToEncode)
+}
+
+func _encodeList(listToEncode []interface{}) string {
+	listString := "l"
+	for _, value := range listToEncode {
+		encodedValue, err := encodeData(value)
+		if err != nil {
+			panic(err)
+		}
+		listString += encodedValue
+	}
+	return listString + "e"
+}
+
+func _encodeDict(dictToEncode map[string]interface{}) string {
+	dictString := "d"
+	keys := make([]string, 0, len(dictToEncode))
+	for key := range dictToEncode {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		value, ok := dictToEncode[key]
+		if !ok {
+			panic("Invalid dict")
+		}
+		dictString += _encodeString(key)
+		encodedValue, err := encodeData(value)
+		if err != nil {
+			panic(err)
+		}
+		dictString += encodedValue
+	}
+	return dictString + "e"
+}
+
+func encodeData(itemToEncode interface{}) (string, error) {
+	switch typedItem := itemToEncode.(type) {
+	case string:
+		return _encodeString(typedItem), nil
+	case int:
+		return _encodeInteger(typedItem), nil
+	case []interface{}:
+		return _encodeList(typedItem), nil
+	case map[string]interface{}:
+		return _encodeDict(typedItem), nil
+	default:
+		return "", fmt.Errorf("unsupported type")
 	}
 }
 
@@ -138,23 +192,31 @@ func main() {
 			panic(err)
 		}
 
-		decoded, _, err := decodeBencodeData(string(fileBytes))
+		decoded, _, err := _decodeDict(string(fileBytes))
 		if err != nil {
 			panic(err)
 		}
 
-		var torrentFile TorrentFile
-		marshalled, err := json.Marshal(decoded)
+		trackerUrl, ok := decoded["announce"].(string)
+		if !ok {
+			panic("Invalid torrent file")
+		}
+		fmt.Printf("Tracker URL: %s\n", trackerUrl)
+		info, ok := decoded["info"].(map[string]interface{})
+		if !ok {
+			panic("Invalid torrent file")
+		}
+		length, ok := info["length"].(int)
+		if !ok {
+			panic("Invalid torrent file")
+		}
+		fmt.Printf("Length: %d\n", length)
+
+		encodedInfo, err := encodeData(info)
 		if err != nil {
 			panic(err)
 		}
-
-		err = json.Unmarshal(marshalled, &torrentFile)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Printf("Tracker URL: %s\nLength: %d", torrentFile.Announce, torrentFile.Info.Length)
+		fmt.Printf("Info hash: %x\n", sha1.Sum([]byte(encodedInfo)))
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
